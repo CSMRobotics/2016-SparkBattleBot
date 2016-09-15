4bed7820f4d5006347b5b35bedbfead97a27c053
infinite-void <jwiens3141@gmail.com>
2016-09-14 20:54:37 -0600

    Added Arduino Code for the 2016 Battlebot

---
+++ Battlebot/Battlebot.ino
@@ -0,0 +1,20 @@
+const int inputPin = A1;
+const int outputPin = 9;
+
+int motorInput;
+
+void setup() {
+  pinMode(outputPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  motorInput = pulseIn(inputPin, HIGH);
+
+  Serial.println(motorInput);
+  if(motorInput < 1500) {
+    digitalWrite(outputPin, LOW);
+  } else {
+    digitalWrite(outputPin, HIGH);
+  }
+}
